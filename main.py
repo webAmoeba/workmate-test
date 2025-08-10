08890b51617d035c8c40a4e499c4a02b0ef1a224
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-08-11 03:14:19 +0400

    feat: add command-line argument for file input and update script entry point

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import argparse
+from pathlib import Path
+
+
 def main():
-    print("Hello from workmate-test!")
+    parser = argparse.ArgumentParser(
+        prog="workmate",
+    )
+    parser.add_argument("--file", required=True, help="Path to file")
+    args = parser.parse_args()
+
+    path = Path(args.file)
+
+    f = open(path, "r", encoding="utf-8")
+    for line in f:
+        print(line, end="")
+    f.close()
 
 
 if __name__ == "__main__":
