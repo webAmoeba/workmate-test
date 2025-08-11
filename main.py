ba4eb1325a27dde937a9d606e854f024965ec37e
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-08-11 15:19:11 +0400

    feat: implement reading several files

--- main.py
+++ main.py
@@ -5,16 +5,25 @@ from pathlib import Path
 def main():
     parser = argparse.ArgumentParser(
         prog="workmate",
+        description="Process JSON log files and generate a report",
+        epilog="""Example:
+        workmate --file example1.log example2.log
+        """,
     )
-    parser.add_argument("--file", required=True, help="Path to file")
+    parser.add_argument(
+        "--file",
+        required=True,
+        nargs="+",
+        metavar="LOG_PATH",
+        help="- Path to log file(s)",
+    )
     args = parser.parse_args()
 
-    path = Path(args.file)
-
-    f = open(path, "r", encoding="utf-8")
-    for line in f:
-        print(line, end="")
-    f.close()
+    for file_name in args.file:
+        path = Path(file_name)
+        with open(path, "r", encoding="utf-8") as f:
+            for line in f:
+                print(line, end="")
 
 
 if __name__ == "__main__":
